33b4b4e8d8bbbf78d68cc83204c664d1e27fe985
Byeonghun Kim <byeonghun.kim1122@gmail.com>
2024-09-06 21:43:48 -0500

    graph

--- Graph_example.py
+++ Graph_example.py
@@ -1,8 +1,8 @@
 '''
 Graph의 방식
 
-1. 인접 행렬 방식
-2. 인접 리스트 방식
+1. 인접 행렬 방식(위치와 거리)
+2. 인접 리스트 방식(위치와 거리, 위치만)
 '''
 
 
@@ -35,8 +35,19 @@ graph_list_2[0].append((4, 9))
 graph_list_2[2].append((0, 5))
 graph_list_2[4].append((0, 9))
 
+graph_list_3 = [[] for _ in range(5)]
+
+graph_list_3[0].append(2)
+graph_list_3[0].append(4)
+graph_list_3[2].append(0)
+graph_list_3[4].append(0)
+
+
+
 print(graph_array)
 print()
 print(graph_list)
 print()
 print(graph_list_2)
+print()
+print(graph_list_3)
